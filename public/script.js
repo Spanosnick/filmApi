12e3499c1bc7552d1caf0c14da88f129ea79a87e
Spanos <nickspanosf1@gmail.com>
2024-10-21 16:10:08 +0300

    not the right way to hide the api key

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const tmdbKey = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OWE5OGE5ZDZhNjhjYmQyZDkwYjAxNWU1NDQ5N2ZmOCIsIm5iZiI6MTcyOTQyNzkyMi41MjEwNjgsInN1YiI6IjYxYjc3OTVkZmE0MDQ2MDA2MDUzNzViNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.03bu50pQuXRF7U8ggW6P6JiS5sjPEhE3VMwKoWPl-nI';
+const tmdbKey = config.MY_KEY
 const tmdbBaseUrl = 'https://api.themoviedb.org/3/';
 const playBtn = document.getElementById('playBtn');
 const options = {
